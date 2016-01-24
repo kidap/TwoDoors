efc9b52230e3380dcc688c98411a9f7c2efd8908
Karlo Pagtakhan <kidap@yahoo.com>
2016-01-24 16:31:40 -0500

    final and working version. code was corrected.

--- TwoDoors/main.c
+++ TwoDoors/main.c
@@ -11,34 +11,35 @@
 int main(int argc, const char * argv[]) {
     
     char play;
-    char choice;
+    int choice; //change to int
     
     printf("Welcome to Two doors.\n");
     printf("Would you like to play? (y/n): ");
-    scanf("%d", &play);
+    scanf("%c", &play); //change %d to %c
     
-    if (play = 'y') {
+    if (play == 'y') { //change = to ==
         
-        print("\nYou are a prisoner in a room with 2 doors and 2 guards.\n");
-        print("One of the doors will guide you to freedom and behind the other is a hangman --you don't know which is which.\n");
-        print("One of the guards always tells the truth and the other always lies. You don't know which one is the truth-teller or the liar either.\n");
-        print("You have to choose and open one of these doors, but you can only ask a single question to one of the guards.\n");
-        print("What do you ask so you can pick the door to freedom?\n\n");
-        print("\t1.Ask the truth-guard to point to the door of doom.\n");
-        print("\t2.Ask the liar-guard to point to the door of doom.\n");
-        print("\t3.Doesn't matter which one you pick.\n");
-        scan("%f", choice);
+        //change print to printf
+        printf("\nYou are a prisoner in a room with 2 doors and 2 guards.\n");
+        printf("One of the doors will guide you to freedom and behind the other is a hangman --you don't know which is which.\n");
+        printf("One of the guards always tells the truth and the other always lies. You don't know which one is the truth-teller or the liar either.\n");
+        printf("You have to choose and open one of these doors, but you can only ask a single question to one of the guards.\n");
+        printf("What do you ask so you can pick the door to freedom?\n\n");
+        printf("\t1.Ask the truth-guard to point to the door of doom.\n");
+        printf("\t2.Ask the liar-guard to point to the door of doom.\n");
+        printf("\t3.Doesn't matter which one you pick.\n");
+        scanf("%d", &choice);//change scan to scanf,%f to %c, add & before choice
         
-        char answer = "No matter which one you choose the guards both tell you which door leads to death, and therefore you can pick the other door.\n";
+        char answer[150] = "No matter which one you choose the guards both tell you which door leads to death, and therefore you can pick the other door.\n";//change to string
         switch (choice) {
             case 1:
-                print("c", answer);
+                printf("%s", answer);//change print to printf and change c to %s
                 break;
             case 2:
-                print("c", answer);
+                printf("%s", answer);//change print to printf and change c to %s
                 break;
             case 3:
-                print("c", answer);
+                printf("%s", answer);//change print to printf and change c to %s
                 break;
             default:
                 break;
