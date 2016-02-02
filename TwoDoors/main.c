d56e66008d1aba3a57392cfdfe7056521eac75de
Karlo Pagtakhan <kidap@yahoo.com>
2016-02-02 10:19:44 -0500

    Allow user to play again

--- TwoDoors/main.c
+++ TwoDoors/main.c
@@ -12,39 +12,50 @@ int main(int argc, const char * argv[]) {
     
     char play;
     int choice; //change to int
+    int continueplaying = 1;
     
     printf("Welcome to Two doors.\n");
     printf("Would you like to play? (y/n): ");
-    scanf("%c", &play); //change %d to %c
+    scanf("%s", &play); //change %d to %s
     
     if (play == 'y') { //change = to ==
         
-        //change print to printf
-        printf("\nYou are a prisoner in a room with 2 doors and 2 guards.\n");
-        printf("One of the doors will guide you to freedom and behind the other is a hangman --you don't know which is which.\n");
-        printf("One of the guards always tells the truth and the other always lies. You don't know which one is the truth-teller or the liar either.\n");
-        printf("You have to choose and open one of these doors, but you can only ask a single question to one of the guards.\n");
-        printf("What do you ask so you can pick the door to freedom?\n\n");
-        printf("\t1.Ask the truth-guard to point to the door of doom.\n");
-        printf("\t2.Ask the liar-guard to point to the door of doom.\n");
-        printf("\t3.Doesn't matter which one you pick.\n");
-        scanf("%d", &choice);//change scan to scanf,%f to %c, add & before choice
+        while (continueplaying == 1) {
+            //change print to printf
+            printf("\nYou are a prisoner in a room with 2 doors and 2 guards.\n");
+            printf("One of the doors will guide you to freedom and behind the other is a hangman --you don't know which is which.\n");
+            printf("One of the guards always tells the truth and the other always lies. You don't know which one is the truth-teller or the liar either.\n");
+            printf("You have to choose and open one of these doors, but you can only ask a single question to one of the guards.\n");
+            printf("What do you ask so you can pick the door to freedom?\n\n");
+            printf("\t1.Ask the truth-guard to point to the door of doom.\n");
+            printf("\t2.Ask the liar-guard to point to the door of doom.\n");
+            printf("\t3.Doesn't matter which one you pick.\n");
+            scanf("%d", &choice);//change scan to scanf,%f to %c, add & before choice
         
-        char answer[150] = "No matter which one you choose the guards both tell you which door leads to death, and therefore you can pick the other door.\n";//change to string
-        switch (choice) {
-            case 1:
-                printf("%s", answer);//change print to printf and change c to %s
-                break;
-            case 2:
-                printf("%s", answer);//change print to printf and change c to %s
-                break;
-            case 3:
-                printf("%s", answer);//change print to printf and change c to %s
-                break;
-            default:
-                break;
+            char answer[150] = "No matter which one you choose the guards both tell you which door leads to death, and therefore you can pick the other door.\n";//change to string
+            switch (choice) {
+                case 1:
+                    printf("%s", answer);//change print to printf and change c to %s
+                    break;
+                case 2:
+                    printf("%s", answer);//change print to printf and change c to %s
+                    break;
+                case 3:
+                    printf("%s", answer);//change print to printf and change c to %s
+                    break;
+                default:
+                    break;
+            }
+        
+            printf("\nWould you like to play again? (y/n): ");
+            scanf("%s", &play); //change %d to %s
+            
+            if (play == 'y'){
+                continueplaying = 1;
+            } else {
+                continueplaying = 0;
+            }
         }
     }
-    
     return 1;
 }
